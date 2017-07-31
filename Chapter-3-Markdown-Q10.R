54d43b31f25458f76a289813efe789a43583be5a
aj9253 <a.jain@utexas.edu>
2017-07-31 01:31:54 -0800

    Finishes Q10

---
+++ Chapter-3-Markdown-Q10.R
@@ -0,0 +1,19 @@
+---
+title: "ISLR: Chapter 3: Q10"
+author: "Akhilesh Jain"
+date: "July 30, 2017"
+output:
+html_document: default
+---
+  
+```{r global_options, include=FALSE}
+knitr::opts_chunk$set(fig.width=8, fig.height=6, fig.path='Figs/',
+                      echo=TRUE, warning=FALSE, message=FALSE)
+```
+
+## Load Libraries
+
+```{r load library , results="hide"}
+library("MASS")
+library("ISLR")
+```
